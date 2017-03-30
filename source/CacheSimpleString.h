ed96fa695383bb14ec1d194f773fed98bda44495
Rubens <rubens_olv@hotmail.com>
2017-03-30 16:14:26 -0300

    Inclusão das classes de CacheString e GAB's com captura de exemplos de combinações.

---
+++ source/CacheSimpleString.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "Common.h"
+//#include "CacheKeySimple.h"
+//#include "CacheKeySimpleHash.h"
+#include <unordered_map>
+#include "UnitScriptData.h"
+
+
+namespace SparCraft { 
+    
+    class CacheKeySimple {
+        
+    protected:
+
+    public:
+        std::vector<IDType> scriptVector;
+        CacheKeySimple();
+        CacheKeySimple(const CacheKeySimple& orig);
+        virtual ~CacheKeySimple();
+        void clear();
+        const bool operator==(const CacheKeySimple &cacheKey) const;
+        void readScriptData(UnitScriptData & currentScript, const IDType& player);
+
+    private:
+
+    };
+    
+    class CacheKeySimpleHash {
+    public:
+        CacheKeySimpleHash();
+        CacheKeySimpleHash(const CacheKeySimpleHash& orig);
+        virtual ~CacheKeySimpleHash();
+        size_t operator()(const CacheKeySimple & cacheKey) const;
+    private:
+
+    };
+   
+    class CacheSimple {
+    protected:
+        std::unordered_map<CacheKeySimple, ScoreType, CacheKeySimpleHash> cache;
+    public:
+        CacheSimple();
+        CacheSimple(const CacheSimple& orig);
+        virtual ~CacheSimple();
+        ScoreType hitItemCache(UnitScriptData & playerScript, const IDType & player);
+        void addItemCache(UnitScriptData & playerScript, const IDType & player, ScoreType LTD2Value);
+        
+    private:
+        
+
+    };
+    
+    
+
+}
+
+
+
