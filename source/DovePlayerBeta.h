9598f48fcae116f9d979a1132c48590e966e09c8
Rubens <rubens_olv@hotmail.com>
2017-04-15 23:47:15 -0300

    Término do desenvolvimento da Classe DovePlayerBeta.

--- source/DovePlayerBeta.h
+++ source/DovePlayerBeta.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdlib.h>
+#include <cmath>
 #include "Player.h"
 #include "PortfolioGreedySearchCache.h"
 #include "PortfolioOnlineEvolutionCache.h"
@@ -15,11 +16,17 @@ namespace SparCraft {
         PortfolioGreedySearchCache * pgs;
         PortfolioOnlineEvolutionCache * poe;
     public:
+        double _costTimePlayout;
         DovePlayerBeta(const IDType & playerID);
         virtual ~DovePlayerBeta();
         void getMoves(GameState & state, const MoveArray & moves, std::vector<Action> & moveVec);
     private:
-
+        bool canApplyExaustSearch(GameState & state);
+        void executeDovetailing(GameState & state, const MoveArray & moves, std::vector<Action> & moveVec);
+        void executeExaustSearch(GameState & state, const MoveArray & moves, std::vector<Action> & moveVec);
+        int proxima(int a[], int N, int M);
+        std::vector<UnitScriptData> obtemSeqUnitScriptData(GameState& state,int n, int m);
+        StateEvalScore playUnitScripts(GameState& state, UnitScriptData script); 
     };
 }
 
