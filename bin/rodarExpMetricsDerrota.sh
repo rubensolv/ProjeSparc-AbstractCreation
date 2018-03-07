a6c96181cc74dd9301de3c55a9c71b4be71348fa
rubensolv <rubens_olv@hotmail.com>
2018-03-07 14:19:46 -0300

    Removing unnecessary settings and files.

--- bin/rodarExpMetricsDerrota.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-for (( i = 76; i <= 100; i++ )); do
-	echo "Iniciando teste"${i}"\n"
-	./SparCraft ../sample_experiment/sample_exp_linux_derrota.txt >> estados_derrota/estado_${i}_der &
-	sleep 1500	
-done
--- bin/rodarExpMetricsVitoria.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-for (( i =76; i <= 100; i++ )); do
-	echo "Iniciando teste"${i}"\n"
-	./SparCraft ../sample_experiment/sample_exp_linux_vitoria.txt >> estados_vitoria/estado_${i}_vic &
-	sleep 6000	
-done
